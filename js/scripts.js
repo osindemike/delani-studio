92ff03627cf97390151b933b3d996a7a47e98467
OsindeMike <michael.osinde@student.moringaschool.com>
2022-01-03 11:04:57 +0300

    add toggle functions

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $(".design, .par1").click(function(){
+    $("#design").toggle();
+    $(".par1").toggle();
+  });
+});
+$(document).ready(function(){
+  $(".development, .par2").click(function(){
+    $("#development").toggle();
+    $(".par2").toggle();
+  });
+});
+$(document).ready(function(){
+  $(".prodmgmt, .par3").click(function(){
+    $("#prodmgmt").toggle();
+    $(".par3").toggle();
+  });
+});

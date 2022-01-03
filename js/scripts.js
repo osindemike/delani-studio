141e9caefebfd0c691ec9cd697d74cadf88d2cc2
OsindeMike <michael.osinde@student.moringaschool.com>
2022-01-03 12:46:40 +0300

    add mouseover and mouseout functions

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,59 @@ $(document).ready(function(){
     $(".par3").toggle();
   });
 });
+$(document).ready(function(){
+  $("#workone").mouseover(function(){
+    $("#slide1").show();
+  }).mouseout(function(){
+    $("#slide1").hide();
+  });
+  });
+$(document).ready(function(){
+  $("#worktwo").mouseover(function(){
+      $("#slide2").show();
+  }).mouseout(function(){
+      $("#slide2").hide();
+  });
+  });
+$(document).ready(function(){
+  $("#workthree").mouseover(function(){
+      $("#slide3").show();
+  }).mouseout(function(){
+      $("#slide3").hide();
+  });
+  });
+$(document).ready(function(){
+  $("#workfour").mouseover(function(){
+      $("#slide4").show();
+  }).mouseout(function(){
+      $("#slide4").hide();
+  });
+  });
+$(document).ready(function(){
+  $("#workfive").mouseover(function(){
+      $("#slide5").show();
+  }).mouseout(function(){
+      $("#slide5").hide();
+  });
+  });
+$(document).ready(function(){
+  $("#worksix").mouseover(function(){
+      $("#slide6").show();
+  }).mouseout(function(){
+      $("#slide6").hide();
+  });
+  });
+$(document).ready(function(){
+  $("#workseven").mouseover(function(){
+        $("#slide7").show();
+  }).mouseout(function(){
+        $("#slide7").hide();
+  });
+  });
+$(document).ready(function(){
+  $("#workeight").mouseover(function(){
+          $("#slide8").show();
+  }).mouseout(function(){
+          $("#slide8").hide();
+  });
+  });
